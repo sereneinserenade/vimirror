17f1a137c8f7fadf9ad8403cfe20aa1470fd9386
Jeet Mandaliya <45892659+sereneinserenade@users.noreply.github.com>
2022-04-09 06:38:37 +0200

    changing the mode to `/vimirror/`

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig(({ mode }) => {
   return {
     plugins: [vue()],
-    base: mode === 'production' ? '/vimirror' : '/'
+    base: mode === 'production' ? '/vimirror/' : '/'
   }
 })
